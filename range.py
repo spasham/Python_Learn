576bd4291bad773f5648068f067f5f7475ebe2bd
SHIVA PASHAM <spasham2@apsrd9489.uhc.com>
2017-10-23 08:48:04 -0500

    updated my first python script to the github

---
+++ range.py
@@ -0,0 +1,13 @@
+#This script is to check if the given number in the given range or not
+
+def check_ran(num,low,high):
+	
+	if low <= num <= high:
+		print "The num ins range", num
+
+
+	else:
+		print "The num is not in range", num
+
+
+
