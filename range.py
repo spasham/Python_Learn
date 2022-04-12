84ce6b5f5086d6dccb16fb6d8909ac0f397c6711
Shiva Kumar Pasham <shivak981@gmail.com>
2022-04-12 18:03:00 +0530

    Update range.py

--- range.py
+++ range.py
@@ -1,13 +1,13 @@
 #This script is to check if the given number in the given range or not
-
+#BLANKLINE
 def check_ran(num,low,high):
-	
+#BLANKLINE	
 	if low <= num <= high:
 		print "The num ins range", num
-
+#BLANKLINE
 
 	else:
 		print "The num is not in range", num
-
+#BLANKLINE
 
 
